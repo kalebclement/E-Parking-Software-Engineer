adedea8296590ea0febc3158b5dfd6962b0f831b
parking management <parkingmanagementsystem2020@gmail.com>
2020-12-14 11:47:10 +0700

    first

--- index.js
+++ index.js
@@ -3,9 +3,16 @@ const app = express();
 const firebase = require('./admin');
 const availability = require('./availability');
 const ticket = require('./ticket');
+const exitparking = require('./exitparking');
 
 app.use(availability);
 app.use(ticket);
+app.use(exitparking);
+
+app.post('/', (req, res) => {
+    res.send("E-PARKING MANAGEMENT SYSTEM API");
+    
+})
 
 app.listen(process.env.PORT || 3000, function(){
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
